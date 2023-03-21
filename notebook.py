261f16a60053fa9571ddb00ef7da8bdfcc2f1e4b
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-03-21 21:22:51 +0000

    - add notebook

---
+++ notebook.py
@@ -0,0 +1,31 @@
+# %%
+import web3
+# %% [markdown]
+"""
+## Question 1
+Get uniswap v3 pool tick spacing for the following contract addresses: WBTC / ETH for
+the fee tiers 0.01%, 0.05%, 0.3% 1%.
+___
+### WBTC / ETH 0.01%
+ - Contract: 0xe6ff8b9A37B0fab776134636D9981Aa778c4e718
+ - Tick spacing: 1
+### WBTC / ETH 0.05%
+ - Contract: 0x4585FE77225b41b697C938B018E2Ac67Ac5a20c0
+ - Tick spacing: 10
+### WBTC / ETH 0.3%
+ - Contract: 0xCBCdF9626bC03E24f779434178A73a0B4bad62eD
+ - Tick spacing: 60
+### WBTC / ETH 1%
+ - Contract: 0x6Ab3bba2F41e7eAA262fa5A1A9b3932fA161526F
+ - Tick spacing: 200
+___
+## Question 2
+Create a time series for the total locked value (TVL) for pools listed in #1 by minute and hour. Also calculate the change in Δ TVL and price between each time interval. Start with the most recent month and then expand the dataset to the starting point of Uniswap v3 (May 5, 2022)
+"""
+# %%
+
+# %%
+
+# %%
+
+# %%
